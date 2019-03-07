8996c6ef3f75d312c562bf22c588e733b17b3f0b
a <a>
2019-03-07 12:10:29 -0600

    direccionamiento a cambió de contraseña con token

--- backend/app/Mail/ResetPasswordMail.php
+++ backend/app/Mail/ResetPasswordMail.php
@@ -1,26 +1,22 @@
 <?php
-
 namespace App\Mail;
-
 use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Queue\ShouldQueue;
-
 class ResetPasswordMail extends Mailable
 {
     use Queueable, SerializesModels;
-
+    public $token;
     /**
      * Create a new message instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($token)
     {
-        //
+        $this->token = $token;
     }
-
     /**
      * Build the message.
      *
@@ -28,6 +24,8 @@ class ResetPasswordMail extends Mailable
      */
     public function build()
     {
-        return $this->markdown('Email.passwordReset');
+        return $this->markdown('Email.passwordReset')->with([
+            'token' => $this->token
+        ]);
     }
 }
